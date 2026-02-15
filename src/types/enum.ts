5b56911a6f242d939c8a071fc576f31dfd810d59
Annas Abdurrahman <annasabdurrahman354@student.uns.ac.id>
2026-02-16 04:14:29 +0700

    change: user role enum value

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -1,6 +1,6 @@
 export enum ROLE {
-  GURU_KEDIRI = "Guru Kediri",
-  GURU_KERTOSONO = "Guru Kertosono",
+  GURU_KEDIRI = "Guru Tes Kediri",
+  GURU_KERTOSONO = "Guru Tes Kertosono",
   ADMIN_KEDIRI = "Sekretaris Tes Kediri",
   ADMIN_KERTOSONO = "Sekretaris Tes Kertosono",
   KOMANDAN = "Komandan",
